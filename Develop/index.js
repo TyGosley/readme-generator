a6d4f5e7c080ee4e7b1da362fc3db85c17ff1eea
TyGosley <tygosley@gmail.com>
2023-02-15 12:11:38 -0800

    installed npm, inquirer.  Included packages index.js.  added files gitnore

--- Develop/index.js
+++ Develop/index.js
@@ -1,4 +1,8 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = ("./utils/generateMarkdown.js");
+
 
 // TODO: Create an array of questions for user input
 const questions = [];
