59bf50fa375aa4f435646e252ea0d875399927d6
TyGosley <tygosley@gmail.com>
2023-02-16 11:34:35 -0800

    added content to generateMarkdown.js

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-// TODO: Create a function that returns a license badge based on which license is passed in
+// ✅ Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   switch (license) {
@@ -10,11 +10,14 @@ function renderLicenseBadge(license) {
   }
 }
 
-// TODO: Create a function that returns the license link
+// ✅ Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  if (!license) {
-    return "";
+  switch (license) {
+    case "":
+      return "";
+    default:
+      break;
   }
 
     let link = "";
@@ -27,7 +30,7 @@ function renderLicenseLink(license) {
         link = "https://www.gnu.org/licenses/gpl-3.0";
         break;
       case "Apache 2.0":
-        link = https://opensource.org/licenses/Apache-2.0;
+        link = "https://opensource.org/licenses/Apache-2.0";
         break;
       default:
         break;
@@ -37,13 +40,60 @@ function renderLicenseLink(license) {
     
 
 
-// TODO: Create a function that returns the license section of README
+// ✅ Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  switch (license) {
+    case "":
+      return "";
+    default:
+      return `## Licenses
+      This project is covered under the ${license} license. To learn more about what this means, click the license button at the top.`;
+  }
+}
+
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
+  ${renderLicenseBadge(data.licesnes)}
+# README Generator
+
+## Description
+${data.description}
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Licenses](#licenses)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## Licenses
+${renderLicenseSection(data.licesnes)}
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+Have questions about this project?  Reach out.
+GitHub: https://github.com/$${data.github}
+Email: ${data.email}
+
+## Credits
+
+
 
 `;
 }
