a59763c22a1999132686b4cf2d69661972760a7c
TyGosley <tygosley@gmail.com>
2023-02-15 20:40:13 -0800

    created fn renderLicenseBadge and Link on markdown.js

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,11 +1,41 @@
 const fs = require("fs");
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch (license) {
+    case "":
+      return "";
+    default:
+      return `![badge](https://img.shields.io/badge/license-${license}-blue)`;
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (!license) {
+    return "";
+  }
+
+    let link = "";
+
+    switch(license) {
+      case "MIT":
+      link = "https://opensource.org/licenses/MIT";
+      break;
+      case "GPLv3":
+        link = "https://www.gnu.org/licenses/gpl-3.0";
+        break;
+      case "Apache 2.0":
+        link = https://opensource.org/licenses/Apache-2.0;
+        break;
+      default:
+        break;
+    }
+    return link;
+  }
+    
+
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
